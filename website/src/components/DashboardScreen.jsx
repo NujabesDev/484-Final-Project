f10373543083eabe9957d331ecde5b1c70a7c5cf
Meleena Torres <mtorr20@uic.edu>
2025-11-28 18:38:36 -0600

    test

--- website/src/components/DashboardScreen.jsx
+++ website/src/components/DashboardScreen.jsx
@@ -133,7 +133,7 @@ export function DashboardScreen({ user }) {
           )}
 
           {/* Center - Title */}
-          <h1 className="text-white text-4xl font-medium tracking-wide mr-29">
+          <h1 className="text-white text-4xl font-medium tracking-wide mr-27">
             {displayedTitle}
             <span className="animate-pulse">|</span>
           </h1>
